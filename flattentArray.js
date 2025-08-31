4ae5d32524a6b410374699b7226760a5dbd2ca88
amit1884 <amitraj200raj@gmail.com>
2025-08-31 20:23:01 +0530

    fixed flatten array code

--- flattentArray.js
+++ flattentArray.js
@@ -1,13 +1,14 @@
-const array=[1,2,[3,4,5,[6,7,8],[9,10],11],12,13]
-
-const flattenArray=(arr,dept=0)=>{
-    let res=[]
-    arr.forEach(el => {
-        if(Array.isArray(el)&&dept>0){
-            res.push(...flattenArray(el,dept--))
-        }else{
-            res.push(el)
-        }
-    });
-    return res
+const flattenArray = (arr, depth = 0) => {
+  let res = []
+  arr.forEach(el => {
+    if (Array.isArray(el) && depth > 0) {
+      res.push(...flattenArray(el, depth - 1))
+    } else {
+      res.push(el)
+    }
+  })
+  return res
 }
+
+const array = [1,2,[3,4,5,[6,7,8],[9,10],11],12,13]
+console.log(flattenArray(array, 1))
