ff7958573c0ec2731a8a3f80a3185326b6f626dc
amit1884 <amitraj200raj@gmail.com>
2025-09-02 19:27:33 +0530

    added basic js output

---
+++ basicOutput.js
@@ -0,0 +1,17 @@
+console.log('1'==[1]) 
+console.log('1,2,3,4'==[1,2,3,4])
+
+console.log('1'===[1])
+console.log('1,2,3,4'===[1,2,3,4])
+
+console.log(typeof null=='object')
+console.log(typeof []=='array') 
+
+
+// OUTPUT
+// true
+// true
+// false
+// false
+// true
+// false. -- ---no type array in js
